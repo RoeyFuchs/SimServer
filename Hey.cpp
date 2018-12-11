46598501f812bfc6dc03310701516ba090609059
Stav <stavkoren123@gmail.com>
2018-12-11 18:03:11 +0200

    bkbkbk

---
+++ Hey.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stav on 12/11/18.
+//
+
+#include "Hey.h"
---
+++ Hey.h
@@ -0,0 +1,14 @@
+//
+// Created by stav on 12/11/18.
+//
+
+#ifndef SIMSERVER_HEY_H
+#define SIMSERVER_HEY_H
+
+
+class Hey {
+
+};
+
+
+#endif //SIMSERVER_HEY_H
