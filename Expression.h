88a8f114d64c92a4807a43d0a6c7d1820342225d
RoeyFuchs <fuchsroey@gmail.com>
2018-12-14 14:00:18 +0200

    add expression interface

---
+++ Expression.h
@@ -0,0 +1,12 @@
+//
+// Created by roey on 12/14/18.
+//
+
+#ifndef SIMSERVER_EXPRESSION_H
+#define SIMSERVER_EXPRESSION_H
+
+class Expression {
+    virtual double execute() = 0;
+};
+
+#endif //SIMSERVER_EXPRESSION_H
