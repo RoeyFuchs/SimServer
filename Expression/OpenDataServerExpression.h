9ef54fe5fcea4575d0767bf4e15f1b954a79e64f
Stav <stavkoren123@gmail.com>
2018-12-15 13:38:22 +0200

    Conect Expression openData server expression parser varExpression

---
+++ Expression/OpenDataServerExpression.h
@@ -0,0 +1,21 @@
+//
+// Created by stav on 12/15/18.
+//
+
+#ifndef SIMSERVER_OPENDATASERVEREXPRESSION_H
+#define SIMSERVER_OPENDATASERVEREXPRESSION_H
+#include "Expression.h"
+
+#endif //SIMSERVER_OPENDATASERVEREXPRESSION_H
+class OpenDataServerExpression: public Expression{
+    double  port;
+    double samplingRate;
+public:
+    OpenDataServerExpression(double port, double samplingRate){
+        this->port= port;
+        this->samplingRate= samplingRate;
+    }
+    virtual double Execute(){
+        return 1;
+    }
+};
